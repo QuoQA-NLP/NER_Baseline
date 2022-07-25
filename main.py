989bce1efe21b5cfb0a725479aa395c55d8f80a7
sangha0411 <sangha110495@gmail.com>
2022-07-25 02:11:58 +0000

    update hyperparameter

--- main.py
+++ main.py
@@ -1,68 +1,97 @@
+import os
+import random
+import torch
 from dataset import label_tokens_ner  # custom function
 from metric import compute_metrics
 import numpy as np
 from transformers import (
     AutoTokenizer,
-    AutoModelForTokenClassification,
+    # AutoModelForTokenClassification,
     TrainingArguments,
     Trainer,
     DataCollatorForTokenClassification,
 )
 
-from datasets import load_dataset
-import wandb
-from easydict import EasyDict
 import yaml
+import wandb
+from dotenv import load_dotenv
+from datasets import load_dataset
+from easydict import EasyDict
+from model import RobertaForTokenClassification
 
-# Read config.yaml file
-with open("config.yaml") as infile:
-    SAVED_CFG = yaml.load(infile, Loader=yaml.FullLoader)
-    CFG = EasyDict(SAVED_CFG["CFG"])
+def main() :
+    # Read config.yaml file
+    with open("config.yaml") as infile:
+        SAVED_CFG = yaml.load(infile, Loader=yaml.FullLoader)
+        CFG = EasyDict(SAVED_CFG["CFG"])
 
-tokenizer = AutoTokenizer.from_pretrained(CFG.PLM)
-dataset = load_dataset(CFG.dset_name, CFG.task)
-model = AutoModelForTokenClassification.from_pretrained(CFG.PLM, num_labels=CFG.num_labels)
+    seed_everything(CFG.seed)
+    tokenizer = AutoTokenizer.from_pretrained(CFG.PLM)
+    dataset = load_dataset(CFG.dset_name, CFG.task)
+    model = RobertaForTokenClassification.from_pretrained(CFG.PLM, num_labels=CFG.num_labels)
 
-# Dataset
-dataset = load_dataset(CFG.dset_name, CFG.task)
-tokenized_datasets = dataset.map(label_tokens_ner, batched=False)
-data_collator = DataCollatorForTokenClassification(tokenizer)
+    # Dataset
+    dataset = load_dataset(CFG.dset_name, CFG.task)
+    tokenized_datasets = dataset.map(label_tokens_ner, batched=False)
+    data_collator = DataCollatorForTokenClassification(tokenizer)
 
-# Wandb
-model_name = CFG.PLM.replace("/", "_")
-run_name = f"{model_name}-finetuned-ner"
-wandb.init(entity=CFG.entity_name, project=CFG.project_name, name=run_name)
+    # Wandb
+    model_name = CFG.PLM.replace("/", "_")
 
-# Train & Eval configs
-training_args = TrainingArguments(
-    run_name,
-    num_train_epochs=CFG.num_epochs,
-    per_device_train_batch_size=CFG.train_batch_size,
-    per_device_eval_batch_size=CFG.valid_batch_size,
-    gradient_accumulation_steps=CFG.gradient_accumulation_steps,
-    learning_rate=CFG.learning_rate,
-    weight_decay=CFG.weight_decay,
-    fp16=CFG.fp16,
-    evaluation_strategy=CFG.evaluation_strategy,
-    save_strategy=CFG.save_strategy,
-    save_total_limit=CFG.num_checkpoints,
-    load_best_model_at_end=CFG.load_best_model_at_end,
-)
+    load_dotenv(dotenv_path="wandb.env")
+    WANDB_AUTH_KEY = os.getenv('WANDB_AUTH_KEY')
+    wandb.login(key=WANDB_AUTH_KEY)
 
-wandb.config.update(training_args)
+    run_name = f"{model_name}-finetuned-ner"
+    wandb.init(entity=CFG.entity_name, project=CFG.project_name, name=run_name)
 
-trainer = Trainer(
-    model,
-    training_args,
-    train_dataset=tokenized_datasets["train"],
-    eval_dataset=tokenized_datasets["validation"],
-    data_collator=data_collator,
-    tokenizer=tokenizer,
-    compute_metrics=compute_metrics,
-)
+    # Train & Eval configs
+    training_args = TrainingArguments(
+        run_name,
+        num_train_epochs=CFG.num_epochs,
+        per_device_train_batch_size=CFG.train_batch_size,
+        per_device_eval_batch_size=CFG.valid_batch_size,
+        gradient_accumulation_steps=CFG.gradient_accumulation_steps,
+        learning_rate=CFG.learning_rate,
+        weight_decay=CFG.weight_decay,
+        warmup_ratio=CFG.warmup_ratio,
+        fp16=CFG.fp16,
+        evaluation_strategy=CFG.evaluation_strategy,
+        save_steps=CFG.save_steps,
+        eval_steps=CFG.eval_steps,
+        logging_steps=CFG.logging_steps,
+        save_strategy=CFG.save_strategy,
+        save_total_limit=CFG.num_checkpoints,
+        load_best_model_at_end=CFG.load_best_model_at_end,
+    )
 
-trainer.train()
+    wandb.config.update(training_args)
 
-trainer.evaluate()
+    trainer = Trainer(
+        model,
+        training_args,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["validation"],
+        data_collator=data_collator,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics,
+    )
 
-wandb.finish()
+    # -- Training
+    trainer.train()
+    # -- Evaluating
+    trainer.evaluate()
+
+    wandb.finish()
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+if __name__ == '__main__':
+    main()
---
+++ model.py
@@ -0,0 +1,79 @@
+import math
+from typing import List, Optional, Tuple, Union
+
+import torch
+import torch.utils.checkpoint
+from packaging import version
+from torch import nn
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+
+from transformers.modeling_outputs import TokenClassifierOutput
+from transformers.modeling_utils import PreTrainedModel
+from transformers.models.roberta.configuration_roberta import RobertaConfig
+from transformers.models.roberta.modeling_roberta import RobertaModel, RobertaPreTrainedModel
+
+class RobertaForTokenClassification(RobertaPreTrainedModel):
+    _keys_to_ignore_on_load_unexpected = [r"pooler"]
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        self.post_init()
+
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        head_mask: Optional[torch.FloatTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ) -> Union[Tuple[torch.Tensor], TokenClassifierOutput]:
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        sequence_output = outputs[0]
+
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return TokenClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
